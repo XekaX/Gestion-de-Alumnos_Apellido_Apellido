593c7af2eae3844d73eb2db6278c581e7a24ae5d
XekaX <05ebedoya@gmail.com>
2025-11-11 09:13:15 +0100

    Añadida la clase OpcionAgregarAlumno

---
+++ src/modelo/OpcionAgregarAlumno.java
@@ -0,0 +1,11 @@
+package modelo;
+
+public class OpcionAgregarAlumno {
+    public void ejecutar(GestorAlumnos gestorAlumnos) {
+        // Solicitar los datos del alumno y agregarlo
+        Alumno nuevoAlumno = new Alumno("Juan", 20);
+        gestorAlumnos.agregarAlumno(nuevoAlumno);
+    }
+
+
+}
