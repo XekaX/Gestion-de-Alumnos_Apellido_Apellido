cf86137ab7db567a39625fa2c420894d1eef0a43
Paradox004 <lobo004valienteyt@gmail.com>
2025-11-11 09:15:32 +0100

    Añadida la clase Mostrar Alumnos

---
+++ src/githubby/OpcionMostrarAlumnos.java
@@ -0,0 +1,10 @@
+package githubby;
+
+public class OpcionMostrarAlumnos implements IOpcionMenu {
+    @Override
+    public void ejecutar(GestorAlumnos gestorAlumnos) {
+        // Mostrar la lista de alumnos utilizando GestorAlumnos
+        gestorAlumnos.listarAlumnos();
+    }
+}
+
